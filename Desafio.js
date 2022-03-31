f597435b36eeb3fcfdd2197ebe44bea1a2fb6bdb
queren8 <102763836+queren8@users.noreply.github.com>
2022-03-31 17:19:00 -0300

    Update Desafio.js

--- Desafio.js
+++ Desafio.js
@@ -1,1 +1,8 @@
+let n1 = Number(window.prompt("Digite um número!"))
+let n2 = Number(window.prompt("Digite outro número!"))        
 
+function Soma() {
+    return n1 + n2;
+}
+
+window.alert(`A soma de ${n1} e ${n2} é igual a ${Soma()}`)
